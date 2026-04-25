cb5717a40f2d3d3fec20df711771e5d92a6971f7
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2026-04-25 14:28:42 +0530

    Refactor thumb command with error handling
    Refactor thumb command to handle exceptions and improve button text assignment.

--- AarumiMusic/plugins/bot/thumb.py
+++ AarumiMusic/plugins/bot/thumb.py
@@ -1,11 +1,6 @@
 #
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+# Copyright (C) 2021-2022 by TheAloneteam@Github
 #
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -16,24 +11,33 @@ from AarumiMusic.utils.decorators import language
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["thumb", "thumbnail"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["thumb", "thumbnail"]) & filters.group & ~filters.user(BANNED_USERS))
 @language
 async def thumb_command(client, message: Message, _):
-    thumb_state = await is_thumb_on(message.chat.id)
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="ᴇɴᴀʙʟᴇ" if not thumb_state else "ᴅɪsᴀʙʟᴇ",
-                callback_data=f"THUMBNAILCHANGE"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴄʟᴏsᴇ", callback_data="close"
-            ),
-        ],
-    ]
-    return await message.reply_text(
-        _["thumb_1"],
-        reply_markup=InlineKeyboardMarkup(buttons),
-    )
+    try:
+        thumb_state = await is_thumb_on(message.chat.id)
+
+        button_text = "ᴇɴᴀʙʟᴇ" if not thumb_state else "ᴅɪsᴀʙʟᴇ"
+
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    text=button_text,
+                    callback_data="THUMBNAILCHANGE"
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="ᴄʟᴏsᴇ",
+                    callback_data="close"
+                ),
+            ],
+        ]
+
+        await message.reply_text(
+            _["thumb_1"],
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+
+    except Exception as e:
+        await message.reply_text(f"❌ Error: {str(e)}")
