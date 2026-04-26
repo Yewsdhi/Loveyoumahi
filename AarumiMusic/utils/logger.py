550e2cb6288758164123d8c14d482a9c89c2c139
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2026-04-26 23:16:32 +0530

    Remove copyright notice and update logger format

--- AarumiMusic/utils/logger.py
+++ AarumiMusic/utils/logger.py
@@ -1,18 +1,5 @@
-# -----------------------------------------------
-# 🔸 AarumiMusic Project
-# 🔹 Developed & Maintained by: Aarumi Bots (https://github.com/itsAarumi)
-# 📅 Copyright © 2025 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItsAarumi
-# -----------------------------------------------
-
 from pyrogram.enums import ParseMode
+
 from AarumiMusic import app
 from AarumiMusic.utils.database import is_on_off
 from config import LOGGER_ID
@@ -20,19 +7,19 @@ from config import LOGGER_ID
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        logger_text = f"""<blockquote>
-<b>{app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
+        logger_text = f"""
+<b>❖ {app.mention} ᴘʟᴀʏ ʟᴏɢ</b>
 
-<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
-<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
-<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.chat.username}
+<b>● ᴄʜᴀᴛ ɪᴅ ➠</b> <code>{message.chat.id}</code>
+<b>● ᴄʜᴀᴛ ɴᴀᴍᴇ ➠</b> {message.chat.title}
+<b>● ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.chat.username}
 
-<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
-<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+<b>● ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>
+<b>● ɴᴀᴍᴇ ➠</b> {message.from_user.mention}
+<b>● ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}
 
-<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
-<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}</blockquote>"""
+<b>● ǫᴜᴇʀʏ ➠</b> {message.text.split(None, 1)[1]}
+<b>● sᴛʀᴇᴀᴍᴛʏᴘᴇ ➠</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
