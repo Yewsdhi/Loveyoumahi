b42356560c0ff0166d0ab98eb0c8b7345c718a7a
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-04 18:27:44 +0530

    Update play.py

--- AarumiMusic/utils/inline/play.py
+++ AarumiMusic/utils/inline/play.py
@@ -40,6 +40,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 text=_["CLOSE_BUTTON"],
                 callback_data=f"forceclose {videoid}|{user_id}",
                 style=ButtonStyle.DANGER,
+            )
         ],
     ]
     return buttons
@@ -89,7 +90,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER
+            InlineKeyboardButton(
+                text="▢",
+                callback_data=f"ADMIN Stop|{chat_id}",
+                style=ButtonStyle.DANGER,
         ],
         [
             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20").
@@ -110,7 +114,10 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER
+            InlineKeyboardButton(
+                text="▢",
+                callback_data=f"ADMIN Stop|{chat_id}",
+                style=ButtonStyle.DANGER,
         ],
         [
             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20").
