0fae27157bb30aa53b221a5751620189fbec0b26
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-04 22:33:50 +0530

    Fix syntax errors in InlineKeyboardButton definitions

--- AarumiMusic/utils/inline/play.py
+++ AarumiMusic/utils/inline/play.py
@@ -90,10 +90,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.PRIMARY),
         ],
         [
-            InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.SECONDARY,
+            InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.SECONDARY),
     ),
             InlineKeyboardButton("sᴛᴀᴛᴜs", callback_data="api_status"),
-            InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20". style=ButtonStyle.SECONDARY,
+            InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20". style=ButtonStyle.SECONDARY),
     ),
         ],
         [
@@ -113,10 +113,10 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.PRIMARY),
         ],
         [
-            InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.SECONDARY,
+            InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.SECONDARY),
     ),
             InlineKeyboardButton("sᴛᴀᴛᴜs", callback_data="api_status"),
-            InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20". style=ButtonStyle.SECONDARY,
+            InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20". style=ButtonStyle.SECONDARY),
     ),
         ],
         [
