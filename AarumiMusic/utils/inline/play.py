6740636518b2e2eb10a00885579dd4036fde6b42
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-04 21:27:15 +0530

    Update play.py

--- AarumiMusic/utils/inline/play.py
+++ AarumiMusic/utils/inline/play.py
@@ -79,6 +79,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
+                style=ButtonStyle.PRIMARY,
             )
         ],
         [
